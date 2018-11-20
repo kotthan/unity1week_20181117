88b50af8ff150dab202cd1e27ccb8270f307bbc6
Tamiwo <taimwo.otutama@gmail.com>
2018-11-21 03:05:24 +0900

    update:計算式の表示

--- Assets/Resources/sprites/CalcSystemManager.cs
+++ Assets/Resources/sprites/CalcSystemManager.cs
@@ -7,11 +7,13 @@ public class CalcSystemManager : MonoBehaviour {
 
     public GameObject textCalc;
 
+    private Text formula;
     private int result = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+        formula = textCalc.GetComponent<Text>();
+        CalcClear();
 	}
 	
 	// Update is called once per frame
@@ -20,11 +22,32 @@ public class CalcSystemManager : MonoBehaviour {
 	}
 
     public void AddNum ( int num ){
-        //textCalc.GetComponent<Text>().text += " + " + num;
-        Debug.Log("add " + num);
+        if (result >= 10){
+            CalcClear();
+        }
+        else if (result != 0){
+            formula.text += " + ";
+        }
+        formula.text += num;
+        result += num;
+        Debug.Log("+ " + num + "= " + result );
+        if( result == 10 ){
+            Equal10();
+        }
+        else if( result > 10 ){
+            Over10();
+        }
+    }
+    void Equal10 () {
+        formula.text += " = 10";
     }
 
     void Over10 () {
-        
+        formula.text += " = " + result;
+    }
+
+    void CalcClear() {
+        result = 0;
+        formula.text = "";
     }
 }
