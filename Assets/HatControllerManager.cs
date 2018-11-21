0663e3a7864b4b49372caae26dd5ffa247148f4d
tamiwo <tamiwo.otutama@gmail.com>
2018-11-21 12:44:20 +0900

    update:Hatコントローラー追加

---
+++ Assets/HatControllerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatControllerManager : MonoBehaviour {
+
+    public float minInterval;
+    public float maxInterval;
+
+    private float interval;
+    private float time;
+
+    // Use this for initialization
+    void Start () {
+        SetTimer();
+	}
+
+    void SetTimer() {
+        interval = Random.Range(minInterval, maxInterval);
+        time = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if( time > interval)
+        {
+            Debug.Log("Hat!"+time);
+            SetTimer();
+        }
+	}
+}
