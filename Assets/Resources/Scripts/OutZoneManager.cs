fbb66f73c151adc93dcc407d0a1786992d10c390
kotthan <34789283+kotthan@users.noreply.github.com>
2018-11-22 00:48:13 +0900

    fix:ゲームオーバー判定をプレイヤーからOutZoneへ変更

---
+++ Assets/Resources/Scripts/OutZoneManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutZoneManager : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject gameManager;
+    //public GameObject playerManager;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //衝突処理
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Invader")
+        {
+            gameManager.GetComponent<GameManager>().GameOver();
+            DestroyPlayer();
+
+        }
+    }
+
+    //プレイヤーオブジェクト削除処理
+    public void DestroyPlayer() {
+        Destroy(player.gameObject);
+    }
+
+}
