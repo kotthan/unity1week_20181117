6f59072a45e7b154e0ac240c3394a94fba4ca921
kotthan <34789283+kotthan@users.noreply.github.com>
2018-11-21 20:44:35 +0900

    update:OutZone追加

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject textGameOver;     //「ゲームオーバー」テキスト
+    public GameObject buttons;          //操作ボタン
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //ゲームオーバー処理
+    public void GameOver() {
+        textGameOver.SetActive(true);
+        buttons.SetActive(false);
+    }
+}
