3983c5622d64209bee4ebc1529535add710cbf1a
kotthan <34789283+kotthan@users.noreply.github.com>
2018-11-21 22:42:10 +0900

    update:ゲームオーバー時の処理を追加
    リスタートボタン

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
-    public GameObject textGameOver;     //「ゲームオーバー」テキスト
-    public GameObject buttons;          //操作ボタン
+    public GameObject textGameOver;         //「ゲームオーバー」テキスト
+    public GameObject ButtonsGameover;      //操作ボタン
+    public GameObject InvadersController;   //インベーダーコントローラ
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -21,6 +23,13 @@ public class GameManager : MonoBehaviour {
     //ゲームオーバー処理
     public void GameOver() {
         textGameOver.SetActive(true);
-        buttons.SetActive(false);
+        ButtonsGameover.SetActive(true);
+        InvadersController.SetActive(false);
     }
+
+    //ボタン処理
+    public void Restart() {         // 現在のScene名を取得する         Scene loadScene = SceneManager.GetActiveScene();         // Sceneの読み直し         SceneManager.LoadScene(loadScene.name);     } 
+    public void InRestart(){        Invoke ("Restart", 0.1f);   } 
+
+    public void LoadTitle() {       SceneManager.LoadScene ("TitleScene");  }   public void InLoadTitle() {         Invoke ("LoadTitle", 0.1f);     } 
 }
