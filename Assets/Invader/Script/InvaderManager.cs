37d310d1d202dfe6cecb240b3f73bc147e7aef25
Tamiwo <taimwo.otutama@gmail.com>
2018-11-22 22:25:03 +0900

    update:数字の割合調整

--- Assets/Invader/Script/InvaderManager.cs
+++ Assets/Invader/Script/InvaderManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class InvaderManager : MonoBehaviour {
 
+    public float[] ratio;
     private int value;
     CalcSystemManager calc;
     ScoreManager score;
 
 	// Use this for initialization
 	void Start () {
-        value = Random.Range(1, 9);
+        SetValue();
         GetComponent<TextMesh>().text = value.ToString();
         var calcSystem = GameObject.Find("CalcSystem");
         calc = calcSystem.GetComponent<CalcSystemManager>();
@@ -34,6 +35,22 @@ public class InvaderManager : MonoBehaviour {
             score.Add(value);
             parent.GetComponent<InvadersManager>().AddDestroyList(gameObject);
             calc.AddNum(value);
+        }
+    }
+
+    void SetValue(){
+        float sum=0;
+        foreach ( float r in ratio ){
+            sum += r;
+        }
+        var randNum = Random.Range(0,sum);
+        sum = 0;
+        for (int i = 0; i < ratio.Length; i++ ){
+            sum += ratio[i];
+            if( randNum < sum ){
+                value = i + 1;
+                break;
+            }
         }
     }
 }
