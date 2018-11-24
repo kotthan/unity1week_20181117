9c7afbd77065c0aeeb6fcbed0288f45d86764e72
Tamiwo <taimwo.otutama@gmail.com>
2018-11-24 15:04:39 +0900

    update:cellophane prefab

---
+++ Assets/Resources/Scripts/CellphaneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellphaneManager : MonoBehaviour {
+
+    public Color color;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var sprite = col.gameObject.GetComponent<SpriteRenderer>();
+        if (sprite != null) {
+            sprite.color = color;
+            return;
+        }
+
+        var textMexh = col.gameObject.GetComponent<TextMesh>();
+        if( textMexh != null ){
+            textMexh.color = color;
+            return;
+        }
+    }
+}
