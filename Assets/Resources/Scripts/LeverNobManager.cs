e0dfae8c6c8879503cb1741050edc43761a38e6b
tamiwo <tamiwo.otutama@gmail.com>
2018-12-07 01:03:39 +0900

    update:タッチでレバーが動くように

---
+++ Assets/Resources/Scripts/LeverNobManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LeverNobManager : MonoBehaviour {
+
+    private Vector3 defPos;
+
+	// Use this for initialization
+	void Start () {
+        defPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseDrag()
+    {
+        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(pos.x, defPos.y, defPos.z);
+    }
+}
