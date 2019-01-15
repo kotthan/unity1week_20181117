5b2d625deab1681f56703fcba59696b43f89ebe4
kotthan <34789283+kotthan@users.noreply.github.com>
2019-01-15 22:37:40 +0900

    fix:修正

---
+++ Assets/Resources/Scripts/ScreenRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // 縦
+        Screen.autorotateToPortrait = false;
+        // 左
+        Screen.autorotateToLandscapeLeft = true;
+        // 右
+        Screen.autorotateToLandscapeRight = true;
+        // 上下反転
+        Screen.autorotateToPortraitUpsideDown = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
